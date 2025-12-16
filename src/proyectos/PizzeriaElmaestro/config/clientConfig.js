266634d56b67803764ab55f0abc0e18b886ce5a2
Esteban Madrid <emadrid@workgroup.com.ar>
2025-12-16 15:15:01 -0300

    cambie el numero de wpp

--- src/proyectos/PizzeriaElmaestro/config/clientConfig.js
+++ src/proyectos/PizzeriaElmaestro/config/clientConfig.js
@@ -3,7 +3,7 @@ export const clientConfig = {
   nombre: "Pizzería El Maestro",          // Nombre del local
   tipo: "pizzeria",                     // pizzeria | hamburgueseria | heladeria | etc.
 
-  whatsapp: "5491162123307",           // Teléfono del negocio (formato internacional)
+  whatsapp: "5491132935209",           // Teléfono del negocio (formato internacional)
 
   logo: "/images/elmaestro.png",    // Ruta dentro de /public (ej: public/images/logo-pizzeria.png)
 
