64beaa4fe92bf50f79ec082f1afc1679044399ba
gonzalocopes <gonzalo.copes@hotmail.com>
2026-03-05 20:51:30 -0300

    atr chobma

--- src/proyectos/ChompaDrinks/data/products.js
+++ src/proyectos/ChompaDrinks/data/products.js
@@ -19,51 +19,51 @@ export const combos = [
   { id: "skyy-speed-2", name: "Skyy + 2 speed xl", category: "Combos", price: 16500, img: "/images/combo_skyy_speed_real.png" },
   { id: "skyy-jugos", name: "skyy + 2 jugos", category: "Combos", price: 16500, img: "/images/combo_skyy_cepita.png" },
   { id: "skyy-monster", name: "skyy + 2 monster", category: "Combos", price: 17000, img: "/images/combo_skyy_monster.png" },
-  { id: "smirnoff-speed", name: "smirnoff + 2 speed xl", category: "Combos", price: 15000, img: "/images/placeholder_combo.png" },
-  { id: "smirnoff-jugos", name: "smirnoff + 2 jugos", category: "Combos", price: 15000, img: "/images/placeholder_combo.png" },
-  { id: "smirnoff-monster", name: "sminroff + 2 monster", category: "Combos", price: 16000, img: "/images/placeholder_combo.png" },
+  { id: "smirnoff-speed", name: "smirnoff + 2 speed xl", category: "Combos", price: 15000, img: "/images/smirnoff_raspberry.png" },
+  { id: "smirnoff-jugos", name: "smirnoff + 2 jugos", category: "Combos", price: 15000, img: "/images/smirnoff_raspberry.png" },
+  { id: "smirnoff-monster", name: "sminroff + 2 monster", category: "Combos", price: 16000, img: "/images/smirnoff_raspberry.png" },
 
   // --- Champagne / Vino espumante ---
-  { id: "baron-b-speed", name: "baron b + 1 speed xl", category: "Combos", price: 34000, img: "/images/placeholder_combo.png" },
-  { id: "chandon-speed", name: "chandon + 1 speed xl", category: "Combos", price: 19500, img: "/images/placeholder_combo.png" },
-  { id: "chandon-monster", name: "chandon + 1 monster", category: "Combos", price: 20000, img: "/images/placeholder_combo.png" },
+  { id: "baron-b-speed", name: "baron b + 1 speed xl", category: "Combos", price: 34000, img: "/images/baron_b.png" },
+  { id: "chandon-speed", name: "chandon + 1 speed xl", category: "Combos", price: 19500, img: "/images/chandon.png" },
+  { id: "chandon-monster", name: "chandon + 1 monster", category: "Combos", price: 20000, img: "/images/chandon.png" },
 
   // --- Vino ---
-  { id: "santa-julia-speed", name: "santa julia + 1 speed xl", category: "Combos", price: 13000, img: "/images/placeholder_combo.png" },
-  { id: "tardia-speed", name: "tardía + 1 speed xl", category: "Combos", price: 8000, img: "/images/placeholder_combo.png" },
-  { id: "santa-filomena-manaos", name: "santa Filomena + 1 manaos", category: "Combos", price: 6500, img: "/images/placeholder_combo.png" },
-  { id: "vinas-alvear-manaos", name: "viñas de Alvear + 1 manaos", category: "Combos", price: 6500, img: "/images/placeholder_combo.png" },
+  { id: "santa-julia-speed", name: "santa julia + 1 speed xl", category: "Combos", price: 13000, img: "/images/santa_julia.png" },
+  { id: "tardia-speed", name: "tardía + 1 speed xl", category: "Combos", price: 8000, img: "/images/tardia.png" },
+  { id: "santa-filomena-manaos", name: "santa Filomena + 1 manaos", category: "Combos", price: 6500, img: "/images/santa_filomena.png" },
+  { id: "vinas-alvear-manaos", name: "viñas de Alvear + 1 manaos", category: "Combos", price: 6500, img: "/images/vinas_de_alvear.png" },
 
   // --- Fernet ---
   { id: "fernet-450-coca", name: "fernet 450p + 1 coca", category: "Combos", price: 17000, img: "/images/combo_fernet_coca.png" },
   { id: "fernet-750-coca", name: "fernet 750 + 2 cocas", category: "Combos", price: 24000, img: "/images/combo_fernet_coca.png" },
 
   // --- Gancia ---
-  { id: "gancia-sprite", name: "gancia + Sprite", category: "Combos", price: 12500, img: "/images/placeholder_combo.png" },
+  { id: "gancia-sprite", name: "gancia + Sprite", category: "Combos", price: 12500, img: "/images/gancia_sprite.png" },
 
   // --- Melón ---
-  { id: "melon-speed", name: "melon + 2 speed xl", category: "Combos", price: 13000, img: "/images/placeholder_combo.png" },
+  { id: "melon-speed", name: "melon + 2 speed xl", category: "Combos", price: 13000, img: "/images/melon_speed.png" },
 
   // --- Federico ---
-  { id: "federico-speed", name: "federico + 1 speed xl", category: "Combos", price: 9000, img: "/images/combo_federico_speed.jpg" },
-  { id: "federico-monster", name: "Federico + 1 monster", category: "Combos", price: 9500, img: "/images/placeholder_combo.png" },
+  { id: "federico-speed", name: "federico + 1 speed xl", category: "Combos", price: 9000, img: "/images/federico_speed.png" },
+  { id: "federico-monster", name: "Federico + 1 monster", category: "Combos", price: 9500, img: "/images/federico_monster.png" },
 ];
 
 export const latas = [
-  { id: "grolsch", name: "grolsch", category: "Latas", price: 2800, img: "/images/placeholder_lata.png" },
-  { id: "miller-lata", name: "miller", category: "Latas", price: 2800, img: "/images/placeholder_lata.png" },
+  { id: "grolsch", name: "grolsch", category: "Latas", price: 2800, img: "/images/lata_grolsch.png" },
+  { id: "miller-lata", name: "miller", category: "Latas", price: 2800, img: "/images/lata_miller.png" },
   { id: "brahma-lata", name: "brahma", category: "Latas", price: 2300, img: "/images/bebidas/lata_brahma.jpg" },
   { id: "heineken-lata", name: "heineken", category: "Latas", price: 3000, img: "/images/heineken_473.png" },
-  { id: "andes-rubia", name: "andes rubia", category: "Latas", price: 2500, img: "/images/placeholder_lata.png" },
-  { id: "andes-roja", name: "andes roja", category: "Latas", price: 2500, img: "/images/placeholder_lata.png" },
-  { id: "andes-negra", name: "andes negra", category: "Latas", price: 2500, img: "/images/placeholder_lata.png" },
-  { id: "budweiser-lata", name: "budweiser", category: "Latas", price: 2300, img: "/images/placeholder_lata.png" },
-  { id: "isenbeck", name: "isenbeck", category: "Latas", price: 2300, img: "/images/placeholder_lata.png" },
-  { id: "imperial-golden", name: "imperial golden", category: "Latas", price: 2500, img: "/images/placeholder_lata.png" },
-  { id: "imperial-ipa", name: "imperial ipa", category: "Latas", price: 2500, img: "/images/placeholder_lata.png" },
-  { id: "imperial-apa", name: "imperial apa", category: "Latas", price: 2500, img: "/images/placeholder_lata.png" },
-  { id: "imperial-negra", name: "imperial negra", category: "Latas", price: 2500, img: "/images/placeholder_lata.png" },
-  { id: "warsteiner", name: "warsteiner", category: "Latas", price: 2300, img: "/images/placeholder_lata.png" },
+  { id: "andes-rubia", name: "andes rubia", category: "Latas", price: 2500, img: "/images/lata_andes_rubia.png" },
+  { id: "andes-roja", name: "andes roja", category: "Latas", price: 2500, img: "/images/lata_andes_roja.png" },
+  { id: "andes-negra", name: "andes negra", category: "Latas", price: 2500, img: "/images/lata_andes_negra.png" },
+  { id: "budweiser-lata", name: "budweiser", category: "Latas", price: 2300, img: "/images/lata_budweiser.png" },
+  { id: "isenbeck", name: "isenbeck", category: "Latas", price: 2300, img: "/images/lata_isenbeck.png" },
+  { id: "imperial-golden", name: "imperial golden", category: "Latas", price: 2500, img: "/images/lata_imperial_golden.png" },
+  { id: "imperial-ipa", name: "imperial ipa", category: "Latas", price: 2500, img: "/images/lata_imperial_ipa.png" },
+  { id: "imperial-apa", name: "imperial apa", category: "Latas", price: 2500, img: "/images/lata_imperial_apa.png" },
+  { id: "imperial-negra", name: "imperial negra", category: "Latas", price: 2500, img: "/images/lata_imperial_negra.png" },
+  { id: "warsteiner", name: "warsteiner", category: "Latas", price: 2300, img: "/images/lata_warsteiner.png" },
   { id: "amstel", name: "amstel", category: "Latas", price: 2300, img: "/images/bebidas/amstel_473.png" },
   { id: "scheneider-lata", name: "scheneider", category: "Latas", price: 2300, img: "/images/bebidas/lata_schneider.jpg" },
 ];
